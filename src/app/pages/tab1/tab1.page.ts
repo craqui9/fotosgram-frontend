99ad18811c336b1f6644f7775845bb80168bea14
Daniel <daniru9@gmail.com>
2020-10-02 09:50:14 +0200

    creacion de servicios, interfaces y modulos

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PostsService } from '../../services/posts.service';
+import { Post } from '../../interfaces/interfaces';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit{
 
-  constructor() {}
+  posts: Post[] = [];
+
+  constructor(private postService: PostsService) {}
+
+  ngOnInit(){
+
+    this.postService.getPosts()
+        .subscribe(resp => {
+          console.log(resp);
+          this.posts.push(...resp.posts);
+        });
+
+  }
 
 }
