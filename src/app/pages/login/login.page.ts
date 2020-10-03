7f14b22f663cce7f6093fe21a6cbd4a8b7c934a2
Daniel <daniru9@gmail.com>
2020-10-03 19:43:11 +0200

    login y registro hecho

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -3,6 +3,7 @@ import { NgForm } from '@angular/forms';
 import { IonSlides, NavController } from '@ionic/angular';
 import { UsuarioService } from '../../services/usuario.service';
 import { UiServiceService } from '../../services/ui-service.service';
+import { Usuario } from '../../interfaces/interfaces';
 
 @Component({
   selector: 'app-login',
@@ -13,49 +14,19 @@ export class LoginPage implements OnInit {
 
   @ViewChild('slidePrincipal', {static: true}) slides: IonSlides ;
 
-  avatars = [
-    {
-      img: 'av-1.png',
-      seleccionado: true
-    },
-    {
-      img: 'av-2.png',
-      seleccionado: false
-    },
-    {
-      img: 'av-3.png',
-      seleccionado: false
-    },
-    {
-      img: 'av-4.png',
-      seleccionado: false
-    },
-    {
-      img: 'av-5.png',
-      seleccionado: false
-    },
-    {
-      img: 'av-6.png',
-      seleccionado: false
-    },
-    {
-      img: 'av-7.png',
-      seleccionado: false
-    },
-    {
-      img: 'av-8.png',
-      seleccionado: false
-    },
-];
-
-  avatarSlide = {
-    slidesPerView: 3.5
-  };
-
   loginUser = {
     email: 'daniru9@gmail.com',
     password: 'jajaxddd12345'
   };
+
+  registerUser: Usuario = {
+    email: 'test',
+    password: '123456',
+    nombre: 'Test',
+    avatar: 'av-1.png'
+  }
+
+
 
   constructor(private usuarioService: UsuarioService,
               private navController: NavController,
@@ -82,17 +53,19 @@ export class LoginPage implements OnInit {
 
   }
 
-  registro(fRegistro: NgForm){
+  async registro(fRegistro: NgForm){
 
-    console.log(fRegistro.valid);    
+    if( fRegistro.invalid){return;}
 
-  }
+    const valido = await this.usuarioService.registro(this.registerUser);
 
-  seleccionarAvatar(avatar){
-    
-    this.avatars.forEach(av => av.seleccionado = false);
-
-    avatar.seleccionado = true;
+    if(valido){
+      //navegar al tabs
+      this.navController.navigateRoot('/main/tabs/tab1', {animated: true});
+    }else{
+      //mostrar alerta de usuario y contraseña no correctos
+      this.uiService.alertaInformativa('Ese correo ya existe.');
+    }
 
   }
 
